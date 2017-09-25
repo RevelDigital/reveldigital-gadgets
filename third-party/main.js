d33f64c41a1d95e5dde906c2cff30e11a765702f
Avery knight <knightav@mnstate.edu>
2017-09-25 12:35:53 -0500

    Update main.js

--- third-party/main.js
+++ third-party/main.js
@@ -102,6 +102,12 @@ var HomePage = (function () {
                         console.log(e);
                     }
                 }
+		if (typeof Client != 'undefined') {
+	           Client.callback(JSON.stringify(jsonArray));
+	        }
+	        else {
+	           alert('client was null');
+	        }
                 console.log(JSON.stringify(jsonArray));
                 this.balance = 0;
                 this.clear();
